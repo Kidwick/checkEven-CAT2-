0d0cf4d9afa53638270d11f2eab2be7da4e546ae
Kidwick <115428041+Kidwick@users.noreply.github.com>
2022-12-11 17:21:34 +0300

    Add files via upload

---
+++ CheckEven.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+
+
+void checkEven()
+{
+    //variable declaration
+    int integer,Even;
+    //user input
+    cout<<"Enter an integer:"<<endl;
+    cin>>integer;
+
+    //logic initialisation
+    if (integer=Even)
+    {
+        cout<<"True"<<endl;
+    }
+    else
+    {
+        cout<<"False"<<endl;
+    }
+
+}
+
+int main()
+{
+    checkEven();
+
+
+    return 0;
+}
